50266da7b822e044dc893fbd99c99f058af53302
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-10-13 21:08:21 +0800

    chore: update

--- src/wotDesignUni1/index.ts
+++ src/wotDesignUni1/index.ts
@@ -1,19 +1,21 @@
-import { getComponentMap, getPropsMap } from './mapping'
+import { getComponentsMap, getPropsMap } from './mapping'
+// import directives from '../directives.json'
 
-export function antd4() {
+export function wotDesignUni1() {
   return {
-    uiName: 'antd4',
+    uiName: 'wot-design-uni',
+    lib: 'wot-design-uni',
     map: getPropsMap(),
-    lib: 'antd',
   }
 }
 
-export function antd4Components(isZh: boolean) {
+export function wotDesignUni1Components() {
   return {
-    map: getComponentMap(isZh),
-    isSeperatorByHyphen: false,
-    prefix: '',
-    lib: 'antd',
-    isReact: true,
+    map: getComponentsMap(),
+    isSeperatorByHyphen: true,
+    prefix: 'wd',
+    isReact: false,
+    lib: 'wot-design-uni',
+    // directives: directives.wotDesignUni1,
   }
 }
